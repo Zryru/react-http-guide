1c2d4c88276601cd729862447b259117fea16303
Jorge Del Castillo <jorge.ale91@gmail.com>
2020-03-22 21:07:51 +0100

    interceptors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,21 +9,27 @@ import Axios from "axios";
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   };
 
   componentDidMount() {
-    Axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      //   console.log("response", response);
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map(post => {
-        return {
-          ...post,
-          author: "jsonPlaceholder"
-        };
+    Axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        //   console.log("response", response);
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "jsonPlaceholder"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error => {
+        console.log("error", error);
+        this.setState({ error: true });
       });
-      this.setState({ posts: updatedPosts });
-    });
   }
 
   postSelectedHandler = id => {
@@ -31,16 +37,19 @@ class Blog extends Component {
   };
 
   render() {
-    const posts = this.state.posts.map(post => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        ></Post>
-      );
-    });
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          ></Post>
+        );
+      });
+    }
 
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import Axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+Axios.interceptors.request.use(
+  request => {
+    console.log("interceptors request", request);
+    return request;
+  },
+  error => {
+    console.log("interceptors error", error);
+    return Promise.reject(error);
+  }
+);
+
+Axios.interceptors.response.use(
+  response => {
+    console.log("response", response);
+    return response;
+  },
+  error => {
+    console.log("response error", error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
